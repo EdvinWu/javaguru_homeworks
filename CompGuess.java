4819481c9d1bbad8796a5d2d82dea73796aaf587
Edvin_Wu <e.malisevskis@gmail.com>
2016-02-16 20:07:23 +0200

    ugadayka - zagadayka

---
+++ CompGuess.java
@@ -0,0 +1,65 @@
+package javaGuru.javaguru_homeworks;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
+
+/**
+ * Created by Edwin on 2016.02.16..
+ */
+public class CompGuess {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String answer = "";
+        Random randomGenerator = new Random();
+        System.out.println("List of commands: 'add` - to add 1-3 to comp's number");
+        System.out.println("'reduce' - to reduce comp's number by 1-3");
+        System.out.println("'right' - when comp guessed the number");
+        System.out.println("Think up a number between 0 and 20");
+        int compAnswer = randomGenerator.nextInt(20);
+        do {
+            System.out.println("Is it " + compAnswer);
+            answer = reader.readLine();
+            switch (answer) {
+                case ("add"):
+                    compAnswer += randomGenerator.nextInt(3) + 1;
+                    //System.out.println(compAnswer);
+                    break;
+                case ("reduce"):
+                    compAnswer -= randomGenerator.nextInt(3) + 1;
+                    // System.out.println(compAnswer);
+                    break;
+                case ("right"):
+                    System.out.println("YAY, i guessed it");
+                    break;
+                default:
+                    System.out.println("Such command does not exist");
+            }
+        } while (!"right".equals(answer));
+    }
+}
+/*
+List of commands: 'add` - to add 1-3 to comp's number
+'reduce' - to reduce comp's number by 1-3
+'right' - when comp guessed the number
+Think up a number between 0 and 20
+Is it 7
+add
+Is it 9
+reduce
+Is it 7
+add
+Is it 9
+reduce
+Is it 7
+add
+Is it 10
+reduce
+Is it 8
+right
+YAY, i guessed it
+
+Process finished with exit code 0
+
+ */
---
+++ HumanGuess.java
@@ -0,0 +1,66 @@
+package javaGuru.javaguru_homeworks;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
+
+/**
+ * Created by Edwin on 2016.02.15..
+ */
+public class HumanGuess {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int humGuessNum = 0;
+        Random randomGenerator = new Random();
+        int guessNum = randomGenerator.nextInt(100)+1;
+        //  System.out.println(guessNum);
+        System.out.println("Try to guess. Number are in 1 - 100 interval");
+        // int diff = 0;
+        do {
+            humGuessNum = Integer.parseInt(reader.readLine());
+            if (humGuessNum < 0 || humGuessNum > 100) {
+                System.out.println("wrong entry, please try again");
+            } else {
+                int diff = guessNum - humGuessNum;
+                if (0 < diff && diff < 5) {
+                    System.out.println("your number is less on  1 - 4");
+                } else if (5 <= diff && diff < 10) {
+                    System.out.println("your number is less on  5 - 9");
+                } else if (10 <= diff && diff < 30) {
+                    System.out.println("your number is less on  10 - 29");
+                } else if (30 <= diff) {
+                    System.out.println("difference is too much, guess another number");
+                } else if (-5 < diff && diff < 0) {
+                    System.out.println("your number is bigger on  1 - 4 ");
+                } else if (-10 < diff && diff <= -5) {
+                    System.out.println("your number is bigger on  5 - 9");
+                } else if (-30 < diff && diff <= -10) {
+                    System.out.println("your number is bigger on 10 - 29");
+                } else if (diff <= -30) {
+                    System.out.println("difference is too much, guess another number");
+                }
+            }
+        } while (humGuessNum != guessNum);
+        System.out.println("Congrats , you guessed it");
+    }
+}
+/*
+Try to guess. Number are in 0 - 100 interval
+20
+difference is too much, guess another number
+80
+your number is bigger on  5 - 9
+75
+your number is bigger on  1 - 4
+73
+your number is bigger on  1 - 4
+72
+your number is bigger on  1 - 4
+71
+Congrats , you guessed it
+
+Process finished with exit code 0
+
+ */
